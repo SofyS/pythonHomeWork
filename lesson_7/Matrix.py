58d6f46f6ae7562af0e1a496f4dac33c555a4176
SofyaS <kss9595@mail.ru>
2022-03-15 17:49:49 +0300

    Домашнее задание №7

---
+++ lesson_7/Matrix.py
@@ -0,0 +1,22 @@
+import copy
+
+
+class Matrix:
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __str__(self):
+        s = ''
+        for i in range(len(self.matrix)):
+            s = s + '\t'.join(map(str, self.matrix[i])) + '\n'
+        return s
+
+    def __add__(self, other):
+        if len(self.matrix) != len(other.matrix):
+            return None
+        res = copy.deepcopy(self.matrix)
+        for i in range(len(self.matrix)):
+            for k in range(len(self.matrix[i])):
+                res[i][k] = self.matrix[i][k] + other.matrix[i][k]
+        return Matrix(res)
