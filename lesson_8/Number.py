98a2b362c120f6fcdca4b845dbcb8b2651f65f06
SofyaS <kss9595@mail.ru>
2022-03-17 18:08:25 +0300

    Домашнее задание №8

---
+++ lesson_8/Number.py
@@ -0,0 +1,3 @@
+class NotNumber(ValueError):
+
+    pass
---
+++ lesson_8/StoreMashines.py
@@ -0,0 +1,49 @@
+class StoreMashines:
+
+    def __init__(self, name, price, quantity, number_of_lists, *args):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.numb = number_of_lists
+        self.my_store_full = []
+        self.my_store = []
+        self.my_unit = {'Модель устройства': self.name, 'Цена за ед': self.price, 'Количество': self.quantity}
+
+    def __str__(self):
+        return f'{self.name} цена {self.price} количество {self.quantity}'
+
+    def reception(self):
+        try:
+            unit = input(f'Введите наименование ')
+            unit_p = int(input(f'Введите цену за ед '))
+            unit_q = int(input(f'Введите количество '))
+            unique = {'Модель устройства': unit, 'Цена за ед': unit_p, 'Количество': unit_q}
+            self.my_unit.update(unique)
+            self.my_store.append(self.my_unit)
+            print(f'Текущий список -\n {self.my_store}')
+        except:
+            return f'Ошибка ввода данных'
+
+        print(f'Для выхода - stop, продолжение - Enter')
+        q = input(f'---> ')
+        if q == 'stop' or q == 'Stop':
+            self.my_store_full.append(self.my_store)
+            print(f'Весь склад -\n {self.my_store_full}')
+            return f'Выход'
+        else:
+            return StoreMashines.reception(self)
+
+
+class Printer(StoreMashines):
+    def to_print(self):
+        return f'Количество принтеров {self.numb}'
+
+
+class Scanner(StoreMashines):
+    def to_scan(self):
+        return f'Количество сканеров {self.numb}'
+
+
+class Copier(StoreMashines):
+    def to_copier(self):
+        return f'Количество ксероксов {self.numb}'
